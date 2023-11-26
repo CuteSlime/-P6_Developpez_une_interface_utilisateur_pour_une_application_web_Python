9b67188bdd892a21dbc29c9f11816fdeb604ab5c
Lydian Orlandini <8015348+CuteSlime@users.noreply.github.com>
2023-11-26 20:35:57 +0100

    add function to get best scored movies

--- Site/JS/index.js
+++ Site/JS/index.js
@@ -13,3 +13,18 @@ async function getgenres() {
     genres = genres.map(genre => genre.name);
     return genres
     };
+async function logMovies() {
+    let responseP1 = await fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score");
+    let responseP2 = await fetch("http://localhost:8000/api/v1/titles/?page=2&sort_by=-imdb_score");
+    let movies = await responseP1.json();
+    movies = movies.results;
+
+    let nextMovies = await responseP2.json();
+    nextMovies = nextMovies.results;
+
+    movies = [...movies, ...nextMovies];
+
+    let bestMovies = movies.slice(0, 7);
+
+    document.getElementById('best_rate')
+  };
