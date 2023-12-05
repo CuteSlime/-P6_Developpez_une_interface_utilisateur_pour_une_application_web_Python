770ef1cecaba52b131c2e60f6466e93d75b957d0
Lydian Orlandini <8015348+CuteSlime@users.noreply.github.com>
2023-12-05 22:53:58 +0100

    add Modal and refactor fillCategorie function

--- Site/JS/index.js
+++ Site/JS/index.js
@@ -8,10 +8,9 @@ async function start(){
     best_movies_cat_2 = await getBestMovies(genres[1])
     best_movies_cat_3 = await getBestMovies(genres[2])
     await fillCategorie(best_rated_movies)
-    await fillCategorie(best_movies_cat_1, categorie="cat1", genre=genres[0])
-    await fillCategorie(best_movies_cat_2, categorie="cat2", genre=genres[1])
-    await fillCategorie(best_movies_cat_3, categorie="cat3", genre=genres[2])
-
+    await fillCategorie(best_movies_cat_1, categorie="categorie1", genre=genres[0])
+    await fillCategorie(best_movies_cat_2, categorie="categorie2", genre=genres[1])
+    await fillCategorie(best_movies_cat_3, categorie="categorie3", genre=genres[2])
 }
 async function getGenres() {
     let response = await fetch(API_URL + "genres");
@@ -61,115 +60,105 @@ async function getBestMovies(genre = "") {
     console.log("4",selectedGenre)
     return selectedGenre
 }
-function fillCategorie(movies_list, categorie="best rate", genre=""){
+async function fillCategorie(movies_list, categorie="best_rate", genre=""){
 
-
-    if (categorie == "best rate"){
-        document.getElementById("best_rate").innerHTML = "<header class='title'>" 
+        if (categorie == "best_rate"){
+            let response = await fetch(movies_list[0].url);
+            let bestMovie = await response.json();
+            document.getElementById("best_rate").innerHTML = "<header class='title'>" 
             + "<h1>Les mieux noté</h1>"
             + "</header>"
+            + '<div class="content" id="content_'+categorie+'">'
+            +'</div>'
 
-        movies_list.forEach(movie => {
-            document.getElementById("best_rate").innerHTML += '<a class="movielink" href="'+'#' +'">'
-                + '<article class="movie">'
-                + '<!-- icon on the top left -->'
-                + '<header class="subheader">'
-                + '<svg viewbox="0 0 50 30" class="icon">'
-                + '<use xlink:href="./src/icon/Top_7.svg#icon"/>'
-                + '</svg>'
-                + '</header>'
-                + '<article>'                
-                + '<!-- Main picture of the movie -->'
-                + '<img src="'+ movie.image_url +'" alt="">'
-                + '</article>'
-                + '<!-- title of the movie -->'
-                + '<footer class="subfooter">'
-                + '<h1 class ="">'+movie.title+ '</h1>'
-                + '</footer>'
-                + '</article>'
-                + '</a>'
-        });
-    }
+            document.getElementById("main_movie").innerHTML =  '<div><h1 class ="title">'+bestMovie.title+ '</h1>'
+            + '<button class="play" id="playButton"><p>Lire</p></button>'
+            + '<article>'+'<p>' + bestMovie.description + '</p>'+ '</article></div>' 
+            document.getElementById("main_movie").style.backgroundImage = 'url(' + movies_list[0].image_url + ')'
+        
 
-    if (categorie == "cat1"){
-        document.getElementById("categorie1").innerHTML = "<header class='title'>" 
-            + "<h1> Meilleur film de la catégorie : " + genre + "</h1>"
-            + "</header>"
+          }
+
+
+
+        else{
+            document.getElementById(categorie).innerHTML = "<header class='title'>"
+                + "<h1> Meilleur film de la catégorie : " + genre + "</h1>"
+                + "</header>"
+                + '<div class="content" id="content_'+categorie+'">'
+                +'</div>'
+        }
             
         movies_list.forEach(movie => {
-            document.getElementById("categorie1").innerHTML += '<a class="movielink" href="'+'#' +'">'
-                + '<article class="movie">'
-                + '<!-- icon on the top left -->'
-                + '<header class="subheader">'
-                + '<svg viewbox="0 0 50 30" class="icon">'
-                + '<use xlink:href="./src/icon/Top_7.svg#icon"/>'
-                + '</svg>'
-                + '</header>'
-                + '<article>'                
-                + '<!-- Main picture of the movie -->'
-                + '<img src="'+ movie.image_url +'" alt="">'
-                + '</article>'
-                + '<!-- title of the movie -->'
-                + '<footer class="subfooter">'
-                + '<h1 class ="">'+movie.title+ '</h1>'
-                + '</footer>'
-                + '</article>'
-                + '</a>'
-        });
-    }
+            const movielink = document.createElement("a");
+            movielink.classList.add("movielink");
+            movielink.href = "#";
+            movielink.addEventListener("click", async (event) => {
+                event.preventDefault(); // Prevent the default link behavior
+                let modal = document.getElementById('myModal');
+                let response = await fetch(movie.url);
+                let moviedata = await response.json();
+                modal.style.display = "block";
+                modal.innerHTML = 
+                '<div class="modal-content" style="background-image: url('+ moviedata.image_url+');">'
+                +   '<div class="movie_detail">'
+                +    '<span class="close">&times;</span>'
+                +    '<p class="title_modal">'+moviedata.title +'</p>'
+                +    '<div class="duration"><h1> Durée :</h1>'
+                +    '<p>'+moviedata.duration +'</p></div>'
+                +    '<div class="genre"><h1> Genre :</h1>'
+                +    '<p>'+moviedata.genres +'</p></div>'
+                +    '<div class="imdb_score"><h1> Score IMDB :</h1>'
+                +    '<p>'+moviedata.imdb_score +'</p></div>'
+                +    '<div class="date_published"><h1> Date de sortie :</h1>'
+                +    '<p>'+moviedata.date_published +'</p></div>'
+                +    '<div class="rated"><h1> Classé :</h1>'
+                +    '<p>'+moviedata.rated +'</p></div>'
+                +    '<div class="countries"><h1> Pays d\'origine :</h1>'
+                +    '<p>'+moviedata.countries +'</p></div>'
+                +    '<div class="directors"><h1> Réalisateur :</h1>'
+                +    '<p>'+moviedata.directors +'</p></div>'
+                +    '<div class="actors"><h1> Acteurs :</h1>'
+                +    '<p>'+moviedata.actors +'</p></div>'
+                +    '<div class="worldwide_gross_income"><h1> Revenu au box office :</h1>'
+                +    '<p>'+moviedata.worldwide_gross_income +'</p></div>'
+                +    '<div class="description"><h1> Résumé :</h1>'
+                +    '<p>'+moviedata.description +'</p></div>'
+                +   '</div>'
+                +  '</div>';
 
-    if (categorie == "cat2"){
-        document.getElementById("categorie2").innerHTML = "<header class='title'>" 
-            + "<h1> Meilleur film de la catégorie : " + genre + "</h1>"
-            + "</header>"
-            
-        movies_list.forEach(movie => {
-            document.getElementById("categorie2").innerHTML += '<a class="movielink" href="'+'#' +'">'
-                + '<article class="movie">'
-                + '<!-- icon on the top left -->'
-                + '<header class="subheader">'
-                + '<svg viewbox="0 0 50 30" class="icon">'
-                + '<use xlink:href="./src/icon/Top_7.svg#icon"/>'
-                + '</svg>'
-                + '</header>'
-                + '<article>'                
-                + '<!-- Main picture of the movie -->'
-                + '<img src="'+ movie.image_url +'" alt="">'
-                + '</article>'
-                + '<!-- title of the movie -->'
-                + '<footer class="subfooter">'
-                + '<h1 class ="">'+movie.title+ '</h1>'
-                + '</footer>'
-                + '</article>'
-                + '</a>'
-        });
-    }
+                span = document.getElementsByClassName("close")[0];
+                span.addEventListener("click", (event) => {
+                    modal.style.display = "none";
+                    modal.innerHTML = ""
+                });
+                window.addEventListener("click", (event) => {
+                    if (event.target == modal) {
+                        modal.style.display = "none";
+                      }
+                })
+            });
 
-    if (categorie == "cat3"){
-        document.getElementById("categorie3").innerHTML = "<header class='title'>" 
-            + "<h1> Meilleur film de la catégorie : " + genre + "</h1>"
-            + "</header>"
+            movielink.innerHTML =
             
-        movies_list.forEach(movie => {
-            document.getElementById("categorie3").innerHTML += '<a class="movielink" href="'+'#' +'">'
-                + '<article class="movie">'
-                + '<!-- icon on the top left -->'
-                + '<header class="subheader">'
-                + '<svg viewbox="0 0 50 30" class="icon">'
-                + '<use xlink:href="./src/icon/Top_7.svg#icon"/>'
-                + '</svg>'
-                + '</header>'
-                + '<article>'                
-                + '<!-- Main picture of the movie -->'
-                + '<img src="'+ movie.image_url +'" alt="">'
-                + '</article>'
-                + '<!-- title of the movie -->'
-                + '<footer class="subfooter">'
-                + '<h1 class ="">'+movie.title+ '</h1>'
-                + '</footer>'
-                + '</article>'
-                + '</a>'
-        });
-    }
+            '<article class="movie">'
+            + '<!-- icon on the top left -->'
+            + '<header class="subheader">'
+            + '<svg viewbox="0 0 50 30" class="icon">'
+            + '<use xlink:href="./src/icon/Top_7.svg#icon"/>'
+            + '</svg>'
+            + '</header>'
+            + '<article>'                
+            + '<!-- Main picture of the movie -->'
+            + '<img src="'+ movie.image_url +'" alt="">'
+            + '</article>'
+            + '<!-- title of the movie -->'
+            + '<footer class="subfooter">'
+            + '<h1 class ="">'+movie.title+ '</h1>'
+            + '</footer>'
+            + '</article>'
+            + '</a>'
+            
+            document.getElementById('content_'+categorie).appendChild(movielink);
+    });
 }
-//   'http://localhost:8000/api/v1/titles/?genre=${genre}&sort_by=-imdb_score'
